79e8e7f60df951ebf7a0363633e3d8f85d4e0b30
Talal Al Ajmi <talalajmi98@gmail.com>
2023-05-29 06:41:37 +0400

    Added toast message

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
 
 import App from "./App.jsx";
 import "./App.css";
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <Toaster />
     </Provider>
   </React.StrictMode>
 );
